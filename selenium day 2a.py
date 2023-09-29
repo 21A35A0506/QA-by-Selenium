23b6646a6a3a3a9eec7279b14bbcbfe2eaa291c8
NOOKALA DIVYA DURGA VEERA SAI AVINASH <110119399+21A35A0506@users.noreply.github.com>
2023-09-29 17:31:27 +0530

    Delete selenium day 2a.py

--- selenium day 2a.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
-# Use WebDriver manager to automatically download and manage ChromeDriver
-driver = webdriver.Chrome(ChromeDriverManager().install())
