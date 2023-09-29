4ba6abca7479ac320c66824c6ed28baa2c3dbe4d
NOOKALA DIVYA DURGA VEERA SAI AVINASH <110119399+21A35A0506@users.noreply.github.com>
2023-09-29 17:32:23 +0530

    Delete selenium day 2.py

--- selenium day 2.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-chrome_service=r"C:\\Users\\DELL\\Desktop\\Selenium\\chromedriver-win64\\chromedriver-win64"
-driver=webdriver.Chrome(service=chrome_service)
-driver.get("https://demoqa.com/")
-print(driver.title)
